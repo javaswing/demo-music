56f8bb716823c0a879680c88aa9619dcdb491719
javaswing <>
2020-12-23 19:54:36 +0800

    style: 添加eslint格式化

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App" dsfas>
-     asdf
-    </div>
-  );
+	return <div className="App" />;
 }
 
 export default App;
