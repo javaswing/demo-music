7901559d5abf84122d43134964c1bcaf9197e91b
javaSwing <zxdlovejava@gmail.com>
2020-12-23 23:23:47 +0800

    feat: 调试rem+ html根字体为vw的适配方式

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,42 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-	return (<div className="title">
-    dasdfas
-  </div>);
+  return (
+    <div className="mod_nav">
+      <nav className="mod_nav_list">
+        <a href="javascrip:void(0);" className="mod_nav_list_item">
+          <span className="mod_nav_list_item_logo">
+            <img src="http://jdc.jd.com/img/80" alt="" />
+          </span>
+          <p className="mod_nav_list_item_name">导航入口</p>
+        </a>
+        <a href="javascrip:void(0);" className="mod_nav_list_item">
+          <span className="mod_nav_list_item_logo">
+            <img src="http://jdc.jd.com/img/80" alt="" />
+          </span>
+          <p className="mod_nav_list_item_name">导航入口</p>
+        </a>
+        <a href="javascrip:void(0);" className="mod_nav_list_item">
+          <span className="mod_nav_list_item_logo">
+            <img src="http://jdc.jd.com/img/80" alt="" />
+          </span>
+          <p className="mod_nav_list_item_name">导航入口</p>
+        </a>
+        <a href="javascrip:void(0);" className="mod_nav_list_item">
+          <span className="mod_nav_list_item_logo">
+            <img src="http://jdc.jd.com/img/80" alt="" />
+          </span>
+          <p className="mod_nav_list_item_name">导航入口</p>
+        </a>
+        <a href="javascrip:void(0);" className="mod_nav_list_item">
+          <span className="mod_nav_list_item_logo">
+            <img src="http://jdc.jd.com/img/80" alt="" />
+          </span>
+          <p className="mod_nav_list_item_name">导航入口</p>
+        </a>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
