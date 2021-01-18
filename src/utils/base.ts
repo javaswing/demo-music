6754d401c178732c025b3c2fb3f26e6b9d17f3bb
javaSwing <zxdlovejava@gmail.com>
2021-01-18 13:09:08 +0800

    feat(utils/base): 添加辅助函数

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -13,3 +13,20 @@ export function rem(val?: number) {
     return '';
   }
 }
+
+/**
+ * 检测当前值是否包含undefinded和val
+ * @param val
+ */
+export function isDef<T>(val: T): val is NonNullable<T> {
+  return val !== undefined && val !== null;
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function isFunction(val: unknown): val is Function {
+  return typeof val === 'function';
+}
+
+export function isObject(val: unknown): val is Record<any, any> {
+  return typeof val !== null && typeof val === 'object';
+}
