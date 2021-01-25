6dba9f887c18572d710be4e39cae2defc74ac167
javaSwing <zxdlovejava@gmail.com>
2021-01-25 22:41:53 +0800

    feat: wok in progress

--- src/container/play-list/index.tsx
+++ src/container/play-list/index.tsx
@@ -37,10 +37,29 @@ export default function PlayList(props: PlayListProps) {
         isArrowDown
         zIndex={3}
       />
-      <section
-        className={cls(styles.top, styles.topBg)}
-        style={{ backgroundImage: 'url(//music.163.com/api/img/blur/109951165443713330?param=170y170)' }}
-      ></section>
+      <section className={styles.topBox}>
+        <section
+          className={cls(styles.top, styles.topBg, styles.placeholder)}
+          style={{
+            backgroundImage: 'url(//music.163.com/api/img/blur/109951165661631177?param=170y170',
+          }}
+        ></section>
+        <div className={cls('row', styles.topMain)}>
+          <div className={styles.coverBox}>
+            <img
+              className={styles.img}
+              src="https://p1.music.126.net/xuNuN4G6ZgtwNKgemO07rQ==/109951165661631177.jpg?imageView=1&type=webp&thumbnail=252x0"
+              alt=""
+            />
+          </div>
+          <div className={cls(styles.infoBox, 'flex-1')}>
+            <div className={cls(styles.title, 'txt-ellipsis--l2')}>
+              Hi 这里有一个树洞Hi 这里有一个树洞Hi 这里有一个树洞Hi 这里有一个树洞Hi 这里有一个树洞Hi 这里有一个树洞Hi
+              这里有一个树洞
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
