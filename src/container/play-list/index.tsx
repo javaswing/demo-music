e4acf9deb5ba3600bb8a730276169e77fa7f67a8
javaSwing <zxdlovejava@gmail.com>
2021-01-31 22:21:03 +0800

    feat: add playlist top

--- src/container/play-list/index.tsx
+++ src/container/play-list/index.tsx
@@ -3,9 +3,7 @@ import cls from 'classnames';
 import NavBar from '@/components/nav-bar';
 import Sticky from '@/components/sticky';
 import { getPlayListById } from '@/services';
-import Marquee from '@/components/marquee';
 
-import Loading from '@/components/loading';
 import { PlayListResponse } from './types';
 import styles from './style.module.scss';
 
@@ -30,39 +28,17 @@ export default function PlayList(props: PlayListProps) {
   }, [initData]);
 
   return (
-    <div className={cls(className, styles.container)}>
-      {/* <Sticky offsetTop={40}>
-        <NavBar
-          title={
-            <>
-              <div>nav</div>
-            </>
-          }
-          className={styles.red}
-          isArrowDown
-          zIndex={3}
-        />
-      </Sticky> */}
-      {/* <Marquee>在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。</Marquee> */}
-      <Loading />
-      {/* <NavBar
-        title={
-          <>
-            <div>歌单</div>
-          </>
-        }
-        isArrowDown
-        zIndex={3}
-      />
-      <section className={styles.topBox}>
-        <section
-          className={cls(styles.top, styles.topBg, styles.placeholder)}
+    <div className={cls(className)}>
+      <section className={styles.top}>
+        <div
+          className={styles.topBg}
           style={{
             backgroundImage: 'url(//music.163.com/api/img/blur/109951165661631177?param=170y170',
           }}
-        ></section>
+        ></div>
         <div className={cls('row', styles.topMain)}>
           <div className={styles.coverBox}>
+            <i className={styles.count}>32342434</i>
             <img
               className={styles.img}
               src="https://p1.music.126.net/xuNuN4G6ZgtwNKgemO07rQ==/109951165661631177.jpg?imageView=1&type=webp&thumbnail=252x0"
@@ -76,7 +52,7 @@ export default function PlayList(props: PlayListProps) {
             </div>
           </div>
         </div>
-      </section>*/}
+      </section>
     </div>
   );
 }
